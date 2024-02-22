6850cb841e1297b3706376fe96b0c170a391c1de
simgur <simgur@pm.me>
2024-02-22 20:54:49 +0100

    Add setup.sh script to install Ansible and run playbook

---
+++ setup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Check if ansible is installed
+if ! command -v ansible &> /dev/null
+then
+    echo "Ansible is not installed. Installing..."
+    # Update package lists for upgrades and new package installations
+    sudo apt-get update
+    # Install software-properties-common to get add-apt-repository
+    sudo apt-get install -y software-properties-common
+    # Add ansible PPA
+    sudo add-apt-repository --yes --update ppa:ansible/ansible
+    # Install ansible
+    sudo apt-get install -y ansible
+fi
+
+# Run the playbook
+ansible-playbook setup_netpi.yml
